8eeed9eac9eabb4660c02c822d687f6e55a6e7ee
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-08-27 18:32:28 +0100

    test

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from github import Github
 
 g = Github(os.environ["GH_AUTH_TOKEN"])
 repo = g.get_repo("jodyphelan/piseq")
-print(os.environ["CIRCLE_PULL_REQUEST"])
+
 pr = repo.get_pull(int(os.environ["CIRCLE_PULL_REQUEST"].split("/")[-1]))
 
-commit = list(pr.get_commits())[0]
-print(commit)
-commit.create_comment("hello!!!")
+commit = repo.get_commit(sha=os.environ["CIRCLE_SHA1"])
+
+commit.create_comment("hello %s" % os.environ["CIRCLE_SHA1"])
